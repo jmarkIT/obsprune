619d744afd8e00b92318e289a986fe7ca290a53a
James Mark <james.david.mark@gmail.com>
2024-01-16 16:38:22 -0600

    Catching main up to other changes

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
+	"slices"
 )
 
 func main() {
-	err := filepath.WalkDir("/Users/james/Documents/Notes", visit)
-	fmt.Println(err)
+	allFiles, err := getFiles("/Users/james/Documents/Notes")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var attSlice []string
+	for _, file := range allFiles {
+		file.setAttachments()
+		if file.attachments != nil {
+			attSlice = append(attSlice, file.attachments...)
+		}
+	}
+	allAttachments, err := getFiles("/Users/james/Documents/Notes/03-RESOURCES 📚/Attachments")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, attachment := range allAttachments {
+		if slices.Contains(attSlice, attachment.filename) {
+			fmt.Println(attachment.path)
+		}
+	}
+
 }
