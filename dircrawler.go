ceaca14eb1118bf07092f65ea6664e7f5ee7a706
James Mark <james.david.mark@gmail.com>
2024-01-16 14:39:07 -0600

    Returning obsDoc objects with getFiles

--- dircrawler.go
+++ dircrawler.go
@@ -3,13 +3,19 @@ package main
 import (
 	"io/fs"
 	"path/filepath"
+	"strings"
 )
 
-func getFiles(root string) ([]string, error) {
-	var files []string
+func getFiles(root string) ([]ObsidianDoc, error) {
+	var files []ObsidianDoc
 	err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
 		if !info.IsDir() {
-			files = append(files, path)
+			filename := strings.Split(path, "/")
+			obsDoc := ObsidianDoc{
+				path:     path,
+				filename: filename[len(filename)-1],
+			}
+			files = append(files, obsDoc)
 		}
 		return nil
 	})
